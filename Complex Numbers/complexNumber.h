e3e5e709f59e9a7491401bb8ab9e0c27e2223109
samrabhatti <100955247+samrabhatti@users.noreply.github.com>
2023-03-03 14:13:14 +0500

    Update complexNumber.h

--- Complex Numbers/complexNumber.h
+++ Complex Numbers/complexNumber.h
@@ -1,1 +1,27 @@
+#pragma once
+#include<iostream>
+using namespace std;
 
+class ComplexNum
+{
+private:
+	int real;
+	int imaginary;
+	friend ostream& operator<<(ostream& _cout, const ComplexNum& obj);
+	friend istream& operator>>(istream& _cin, ComplexNum& obj);
+public:
+	ComplexNum();
+	ComplexNum(int r, int i);
+	ComplexNum(const ComplexNum& obj);
+	~ComplexNum();
+	void SetReal(int r);
+	int GetReal();
+	void SetImaginary(int i);
+	int GetImaginary();
+	void Display();
+	ComplexNum operator+ (const ComplexNum& obj);
+	ComplexNum operator- (const ComplexNum& obj);
+	ComplexNum operator = (const ComplexNum& obj);
+	bool operator == (ComplexNum& obj);
+	bool operator != (ComplexNum& obj);
+};
